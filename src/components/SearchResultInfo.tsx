b6f5d8479ee562e879ae9cdcd8a9c022b142f50e
Adeolu David <adeolusegun99@gmail.com>
2024-03-11 00:59:56 +0100

    added the products and product

--- src/components/SearchResultInfo.tsx
+++ src/components/SearchResultInfo.tsx
@@ -7,12 +7,12 @@ type Props = {
 
 const SearchResultInfo = ({ total, city }: Props) => {
   return (
-    <div className="text-xl font-bold flex flex-col gap-3 justify-between lg:items-center lg:flex-row">
+    <div className='text-xl font-bold flex flex-col gap-3 justify-between lg:items-center lg:flex-row'>
       <span>
-        {total} Restaurants found in {city}
+        {total} Store found in {city}
         <Link
-          to="/"
-          className="ml-1 text-sm font-semibold underline cursor-pointer text-blue-500"
+          to='/'
+          className='ml-1 text-sm font-semibold underline cursor-pointer text-blue-500'
         >
           Change Location
         </Link>
